1e42234ebb726c1849bd4b5180ef7432b7e28967
voomp <bat7gamer7@gmail.com>
2021-11-22 18:32:20 -0500

    this caret feature is slowly making me want to kms

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,48 @@
 let currIndex = 0;
-const text = 'hello amogus susy';
 
-const render = () => {
-	$(".words").html('<span id="caret">|</span>'+`<span class="words state-0">${text}</span>`);
+const getDefaultTextData = (text) => {
+	const letters = text.split('');
+	const data = [];
+	for(let i = 0; i < letters.length; i++) {
+		data.push({
+			index: i,
+			state: 0,
+			letter: letters[i]
+		});
+	}
+
+	// console.log(data);
+	return data;
+}
+
+const textData = getDefaultTextData('hello amogus susy');
+
+const renderData = () => {
+
+	console.log(textData);
+	const elementData = [];
+
+	for(const data of textData) {
+		// if(currIndex == data.index) {
+		// 	elementData.push('<span id="caret"></span>');
+		// }
+		elementData.push(`<span class="words state-${data.state}">${data.letter}</span>`);
+	}
+	$(".words").html(elementData.join(""));
+	// $(".words").html('<span id="caret">|</span>'+`<span class="words state-0">${text}</span>`);
 }
 
 const onKeyPress = e => {
-	console.log(e.key);
+	const key = e.code;
+	if((key !== 'Backspace' && !key.includes("Key")) || (currIndex+1>textData.length)) return;
+	console.log(key);
+	textData[currIndex].state = 1;
+	currIndex++;
+	// $("#caret").css('right', `${currIndex*300}px`);
+	document.getElementById("caret").style.right = currIndex;
+	console.log(`${currIndex*300}px`)
+	renderData();
 }
 
 document.addEventListener("keyup", onKeyPress);
-render();
+renderData();
