3923a12188e45bd28361f4e81734a6ff7535c41a
voomp <bat7gamer7@gmail.com>
2021-11-22 18:55:42 -0500

    minimalized code, made rendering faster, underline caret

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,29 @@
 let currIndex = 0;
-
-const getDefaultTextData = (text) => {
-	const letters = text.split('');
-	const data = [];
-	for(let i = 0; i < letters.length; i++) {
-		data.push({
-			index: i,
-			state: 0,
-			letter: letters[i]
-		});
-	}
-
-	// console.log(data);
-	return data;
-}
-
-const textData = getDefaultTextData('hello amogus susy');
+const text = 'hello test test amogus';
 
 const renderData = () => {
 
-	console.log(textData);
-	const elementData = [];
+	const letters = text.split('');
+	const elementData = [`<span class="words state-0 state-curr" id="ind-0">${letters[0]}</span>`];
 
-	for(const data of textData) {
-		// if(currIndex == data.index) {
-		// 	elementData.push('<span id="caret"></span>');
-		// }
-		elementData.push(`<span class="words state-${data.state}" id="ind-${data.index}">${data.letter}</span>`);
+	for(let i = 1; i < letters.length; i++) {
+		elementData.push(`<span class="words state-0" id="ind-${i}">${letters[i]}</span>`);
 	}
+
 	$(".words").html(elementData.join(""));
-	// $(".words").html('<span id="caret">|</span>'+`<span class="words state-0">${text}</span>`);
 }
 
 const onKeyPress = e => {
 	const key = e.code;
-	if((key !== 'Backspace' && !key.includes("Key")) || (currIndex+1>textData.length)) return;
+
+	if(key !== 'Backspace' && !key.includes("Key")) return;
+	if(currIndex+1 > text.length) return;
 	console.log(key);
-	textData[currIndex].state = 1;
+
+	document.getElementById(`ind-${currIndex}`).classList.replace("state-0", "state-1");
+	document.getElementById(`ind-${currIndex}`).classList.remove("state-curr");
 	currIndex++;
-	// $("#caret").css('right', `${currIndex*300}px`);
-	// document.getElementById("caret").style.right = currIndex;
-	// console.log(`${currIndex*300}px`)
-	// renderData();
+	document.getElementById(`ind-${currIndex}`).classList.add("state-curr");
 }
 
 document.addEventListener("keyup", onKeyPress);
