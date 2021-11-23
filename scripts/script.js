c7dafb9aeed4fcbefd8bc48feb2ee91721b3900c
voomp <bat7gamer7@gmail.com>
2021-11-23 17:27:30 -0500

    backspace feature

--- scripts/script.js
+++ scripts/script.js
@@ -17,7 +17,17 @@ const onKeyPress = e => {
 	const code = e.code;
 
 	if(code !== 'Backspace' && code !== 'Space' && !code.includes("Key")) return;
-	if(currIndex+1 > text.length) return;
+	if(currIndex >= text.length) return;
+
+	if(code === 'Backspace' && currIndex > 0) {
+		console.log('hello');
+		document.getElementById(`ind-${currIndex}`).classList.remove("state-curr");
+		currIndex--;
+		document.getElementById(`ind-${currIndex}`).classList.add("state-curr");
+		document.getElementById(`ind-${currIndex}`).classList.replace("state-1", "state-0");
+		document.getElementById(`ind-${currIndex}`).classList.replace("state-2", "state-0");
+		return;
+	}
 	
 	if(e.key === text[currIndex])
 		document.getElementById(`ind-${currIndex}`).classList.replace("state-0", "state-1");
