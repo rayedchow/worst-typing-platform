9f2eb8c37c0acffd6e2843076ab9d35ccf5b5a4d
voomp <bat7gamer7@gmail.com>
2021-11-28 13:01:02 -0500

    language datasets, data routing

---
+++ routers/dataRouter.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const languageRanking = ['english', 'english_5k', 'english_25k', 'english_450k'];
+
+router.post('/getLanguageData', (req, res) => {
+	const { level } = req.body;
+	try {
+		const data = require(`../languageData/${languageRanking[level-1]}.json`);
+		return res.json({
+			level,
+			data
+		});
+	} catch(error) {
+		return res.status(500).json({error});
+	}
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
+app.use(express.json());
+
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/views/index.html");
 });
 
 app.listen(port, () => console.log(`SERVER STARTED ON PORT ${port} (http://localhost:${port})`));
+app.use("/data", require("./routers/dataRouter"));
